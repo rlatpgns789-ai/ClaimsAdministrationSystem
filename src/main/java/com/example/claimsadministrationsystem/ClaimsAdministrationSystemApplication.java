fe0787b4f3f5fa7f537ab4674a2cc4bb0c1f22ef
kams <rlatpgns789@gmail.com>
2026-01-12 21:59:43 +0900

    Initial Commit

---
+++ src/main/java/com/example/claimsadministrationsystem/ClaimsAdministrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.claimsadministrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimsAdministrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimsAdministrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/claimsadministrationsystem/ClaimsAdministrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.claimsadministrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaimsAdministrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
