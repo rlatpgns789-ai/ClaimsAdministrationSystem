0ffca15680b5e93229e1cffbe6c7c5e453a86684
kams <rlatpgns789@gmail.com>
2026-01-15 02:57:52 +0900

    chore : 컨트롤러, 서비스, yml 추가

---
+++ src/main/java/com/example/claimsadministrationsystem/feature/proxyrequest/repository/ProxyRequestRepository.java
@@ -0,0 +1,11 @@
+package com.example.claimsadministrationsystem.feature.proxyrequest.repository;
+
+import com.example.claimsadministrationsystem.domain.ProxyRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ProxyRequestRepository extends JpaRepository<ProxyRequest, Long> {
+
+
+}
---
+++ src/main/java/com/example/claimsadministrationsystem/feature/proxyrequest/service/ProxyRequestService.java
@@ -0,0 +1,11 @@
+package com.example.claimsadministrationsystem.feature.proxyrequest.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProxyRequestService {
+
+
+}
