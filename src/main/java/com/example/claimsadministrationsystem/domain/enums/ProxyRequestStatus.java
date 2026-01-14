800dca8eb31cc361f8a2b7c29e52edae557b7a2c
kams <rlatpgns789@gmail.com>
2026-01-14 22:54:15 +0900

    feat: 상태전이규칙 추가

--- src/main/java/com/example/claimsadministrationsystem/domain/enums/ProxyRequestStatus.java
+++ src/main/java/com/example/claimsadministrationsystem/domain/enums/ProxyRequestStatus.java
@@ -1,5 +1,9 @@
 package com.example.claimsadministrationsystem.domain.enums;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
 public enum ProxyRequestStatus {
 
     PENDING,
@@ -8,5 +12,72 @@ public enum ProxyRequestStatus {
     COMPLETED,
     CANCELLED,
     DISCLAIMER;
+
+    private static final Map<ProxyRequestStatus, Set<ProxyRequestStatus>> ALLOWED_TRANSITIONS =
+            Map.of(
+                    PENDING, Set.of(IN_PROGRESS, CANCELLED, DISCLAIMER),
+                    IN_PROGRESS, Set.of(FEE_CLAIM, DISCLAIMER),
+                    FEE_CLAIM, Set.of(COMPLETED),
+                    COMPLETED, Collections.emptySet(),
+                    CANCELLED, Collections.emptySet(),
+                    DISCLAIMER, Collections.emptySet()
+            );
+
+
+    public void checkIsTerminal(){
+        if (ALLOWED_TRANSITIONS.get(this).isEmpty()){
+            throw new IllegalStateException(
+                    "Cannot transition from terminal status: " + this
+            );
+        }
+    }
+
+    public void checkCannotTransitionTo(ProxyRequestStatus status){
+        Set<ProxyRequestStatus> target = ALLOWED_TRANSITIONS.get(this);
+
+       if (!target.contains(status)){
+           throw new IllegalStateException(
+                   "Invalid status transition order: " + this + " -> " + status
+           );
+       }
+    }
+
+
+    public ProxyRequestStatus progress(){
+        checkIsTerminal();
+        checkCannotTransitionTo(IN_PROGRESS);
+        return IN_PROGRESS;
+
+    }
+
+    
+    public ProxyRequestStatus feeClaim(boolean isPrePaid){
+        checkIsTerminal();
+        checkCannotTransitionTo(FEE_CLAIM);
+        if (isPrePaid){
+            return COMPLETED;
+        } else {
+            return FEE_CLAIM;
+        }
+    }
+
+    public ProxyRequestStatus cancel(){
+        checkIsTerminal();
+        checkCannotTransitionTo(CANCELLED);
+        if (this != PENDING){
+            throw new IllegalStateException("Unexpected value: " + this.name());
+        }
+
+        return CANCELLED;
+    }
+
+    public ProxyRequestStatus disclaimer(){
+        checkIsTerminal();
+        if (this != PENDING && this != IN_PROGRESS){
+            throw new IllegalStateException("Unexpected value: " + this.name());
+        }
+        return DISCLAIMER;
+    }
+
 
 }
